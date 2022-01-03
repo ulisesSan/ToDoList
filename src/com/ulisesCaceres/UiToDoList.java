08c84d386b449c41f7bc7f9048c53fbddfce3400
ulisesSan <uliseo_95@hotmail.com>
2022-01-02 20:54:10 -0600

    Desarrollo de la UI

--- src/com/ulisesCaceres/UiToDoList.java
+++ src/com/ulisesCaceres/UiToDoList.java
@@ -1,14 +1,22 @@
 package com.ulisesCaceres;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class UiToDoList extends JFrame{
     private JPanel UiToDoList;
+    private JButton Agregar;
+    private JTextField textField1;
+    private JTable table1;
+    private JButton Eliminar;
+    private JButton Actualizar;
 
     public UiToDoList(String title){
         super (title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(UiToDoList);
+        this.setMinimumSize(new Dimension(400,200));
+        this.setMaximumSize(new Dimension(400,200));
         this.pack();
     }
 }
